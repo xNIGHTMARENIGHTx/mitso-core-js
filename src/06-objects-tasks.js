5e0250bcb7e5db9f8e8721b190212cfc3c9c35c6
xNIGHTMARENIGHTx <vqy008oj1@gmail.com>
2023-02-09 13:57:25 +0300

    i need what to what in what?(tsk 4 not complete)

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -19,8 +19,18 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  return {
+    width,
+    height,
+    constructor() {
+      this.width = width;
+      this.height = height;
+    },
+    getArea() {
+      return this.width * this.height;
+    },
+  };
 }
 
 /**
@@ -33,8 +43,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -48,8 +58,11 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+
+  return new proto.constructor(...values);
 }
 
 /**
